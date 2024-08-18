ba9023c25ecda6c6b49ba0df58f6825b74a33108
B Kiran <venukiran601@gmail.com>
2024-08-18 12:29:06 +0530

    adding 2st method code

---
+++ Sieve_of_Eratosthenes.py
@@ -0,0 +1,15 @@
+def sieve_of_eratosthenes(limit):
+    primes = [True] * (limit + 1)
+    # print(primes)
+    p = 2
+    while p * p <= limit:
+        if primes[p]:
+            for i in range(p * p, limit + 1, p):
+                primes[i] = False
+        p += 1
+        print(primes)
+    return [p for p in range(2, limit + 1) if primes[p]]
+if sieve_of_eratosthenes(int(input())):
+    print("prime")
+else:
+    print("not prime")
