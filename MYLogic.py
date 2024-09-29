a8a8144400f80c4859c253a431e2c8690fe96325
B Kiran <venukiran601@gmail.com>
2024-09-29 22:18:09 +0530

    Trying my own logic

---
+++ MYLogic.py
@@ -0,0 +1,14 @@
+def prime(n):
+    if n%2==0:
+        return False
+    i = 3
+    while i < n//2:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+n = int(input())
+if prime(n):
+    print("True")
+else:
+    print("False")
