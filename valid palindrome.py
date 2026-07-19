e730da6e13473b4b954edbcf01a20031cab83fb4
B Kiran <117004574+bkiran0745@users.noreply.github.com>
2026-07-19 09:02:10 +0530

    got new idea that without using anothe str
    Refactor palindrome check to use two-pointer technique.

--- valid palindrome.py
+++ valid palindrome.py
@@ -13,8 +13,26 @@ def check(s):
     #     j -= 1
 
     # return True
-  s = "".join(char.lower() for char in s if char.isalnum())
-  return s == s[::-1]
+  # s = "".join(char.lower() for char in s if char.isalnum())
+  # return s == s[::-1]
+  j = len(s)-1
+  ch = 0
+  while ch < j:
+    if s[ch].isalnum() & s[j].isalnum():
+      if s[ch].lower() == s[j].lower():
+        j -= 1
+        ch += 1
+        continue
+      else:
+        print(s[ch],s[j])
+        return False
+    if not s[ch].isalnum():
+      ch += 1
+      continue
+    if not s[j].isalnum():
+      j -= 1
+      continue
+  return True
 
 
 print(check("NoxinNixon"))                 # True
